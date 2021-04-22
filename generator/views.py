a3015289549d2ba5542a6b1917d55c99696cee55
Cameron Stock <clstock33@gmail.com>
2021-04-22 18:46:17 -0500

    created password characters

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
@@ -9,4 +10,13 @@ def home(request):
 
 
 def password(request):
-    return render(request, 'generator/password.html')
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    length = 10
+
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': thepassword})
